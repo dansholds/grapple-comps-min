5ba7203c7f608e7673c28064c7bffdc38908be12
Lisa Major <lisamajor@Lisas-MBP.ultrahub>
2023-07-30 16:47:59 +0100

    removed comps, reverted us nav

--- src/components/header.js
+++ src/components/header.js
@@ -8,11 +8,8 @@ import { useStaticQuery, graphql } from "gatsby";
 const HEADER_NAV_ITEM = [
   {
     label: "Comps",
-    isDropdown: true,
-    dropdownOptions: [
-      { label: "UK", url: "/comps" },
-      { label: "US", url: "/comps-us" },
-    ],
+    url: "/comps",
+    isExternal: false,
   },
   {
     label: "About",
@@ -48,14 +45,6 @@ const Header = () => {
 
         <HeaderNavList>
           {HEADER_NAV_ITEM.map((item, index) => {
-            if (item.isDropdown) {
-              return (
-                <HeaderNavListItem key={index}>
-                  <DropdownNav label={item.label} options={item.dropdownOptions} />
-                </HeaderNavListItem>
-              );
-            }
-
             if (item.isExternal) {
               return (
                 <HeaderNavListItem key={index}>
@@ -83,35 +72,16 @@ const Header = () => {
 
 export default Header;
 
-const DropdownNav = ({ label, options }) => {
-  const [isDropdownOpen, setDropdownOpen] = React.useState(false);
-
-  const handleDropdownToggle = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
-
-  const closeDropdown = () => {
-    setDropdownOpen(false);
-  };
-
+const HeaderNavList = ({ children }) => {
   return (
-    <StyledDropdownNav>
-      <DropdownButton onClick={handleDropdownToggle}>
-        {label} <DropdownIcon open={isDropdownOpen} />
-      </DropdownButton>
-      {isDropdownOpen && (
-        <DropdownMenu>
-          {options.map((option, index) => (
-            <DropdownMenuItem key={index}>
-              <Link to={option.url} onClick={closeDropdown}>
-                {option.label}
-              </Link>
-            </DropdownMenuItem>
-          ))}
-        </DropdownMenu>
-      )}
-    </StyledDropdownNav>
+    <StyledNav>
+      <StyledNavList>{children}</StyledNavList>
+    </StyledNav>
   );
+};
+
+const HeaderNavListItem = ({ children }) => {
+  return <StyledNavListItem>{children}</StyledNavListItem>;
 };
 
 const StyledHeader = styled.header`
@@ -132,18 +102,6 @@ const HeaderTitle = styled.div`
     color: inherit;
   }
 `;
-
-const HeaderNavList = ({ children }) => {
-  return (
-    <StyledNav>
-      <StyledNavList>{children}</StyledNavList>
-    </StyledNav>
-  );
-};
-
-const HeaderNavListItem = ({ children }) => {
-  return <StyledNavListItem>{children}</StyledNavListItem>;
-};
 
 const StyledNav = styled.nav`
   position: static;
@@ -181,72 +139,5 @@ const StyledNavListItem = styled.li`
     & a {
       font-size: 0.7rem;
     }
-  }
-`;
-
-const StyledDropdownNav = styled.div`
-  position: relative;
-`;
-
-const DropdownButton = styled.button`
-  background: transparent;
-  border: none;
-  color: inherit;
-  cursor: pointer;
-  text-transform: uppercase;
-  font-size: var(--size-300);
-  text-decoration: none;
-  letter-spacing: 0.1rem;
-`;
-
-const DropdownIcon = styled.span`
-  display: inline-block;
-  margin-left: 0.5rem;
-  transform: rotate(${(props) => (props.open ? "180deg" : "0deg")});
-  transition: transform 0.2s ease;
-`;
-
-// ... (previously defined components and styles)
-
-const DropdownMenu = styled.ul`
-  position: absolute;
-  top: 100%;
-  left: 0;
-  display: flex;
-  flex-direction: column;
-  padding: 0;
-  list-style-type: none;
-  background-color: #fff;
-  border: 1px solid #ddd;
-  /* Set the width of the dropdown menu to match the width of the parent nav item */
-  width: 100%;
-
-  /* Add box-shadow for a visual effect */
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-
-  /* Optional: You can add additional styles for positioning, etc. */
-  /* For example, you can position the dropdown slightly below the parent nav item */
-  /* top: calc(100% + 5px); */
-  /* Or align the dropdown to the right */
-  /* right: 0; */
-`;
-
-const DropdownMenuItem = styled.li`
-  padding: 0.5rem;
-  /* No need for the border-bottom since the menu width matches the parent nav item */
-  /* &:not(:last-child) {
-    border-bottom: 1px solid #ddd;
-  } */
-  & a {
-    color: inherit;
-    text-decoration: none;
-    /* Set the dropdown item to take up the full width */
-    display: block;
-    width: 100%;
-  }
-  & a:hover {
-    color: #007bff;
-    /* Optional: Add a background color on hover for better user experience */
-    /* background-color: #f5f5f5; */
   }
 `;
