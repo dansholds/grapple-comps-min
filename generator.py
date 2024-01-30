624026903395d7b5fd9794a711654a3c4d6089be
Dan <dan@Dans-MBP-2.ultrahub>
2024-01-30 08:27:43 +0000

    updated generator to take in a list of URLS

--- generator.py
+++ generator.py
@@ -7,53 +7,52 @@ from dateutil import parser
 
 def extract_data(url):
     try:
+        # Fetch HTML content from the URL
         response = requests.get(url)
         response.raise_for_status()
 
+        # Parse HTML content with BeautifulSoup
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        #Get Location & Cost
+        # Extract Location & Cost
         list_item_texts = soup.find_all(class_="sc-list-item-text")
         location = [item.text.strip() for item in list_item_texts]
 
-        #Extract Location
+        # Extract Location
         place = [item.split('\n\n', 1)[1].split()[0] for item in location if '\n\n' in item]
         place = place[0]
 
-        # Get highest price
+        # Extract the highest price
         text = " ".join(location)
         numbers = re.findall(r"(\d+)\s+GBP", text)
         numbers = [int(num) for num in numbers]
 
         # Check if the list is not empty before calculating the maximum
         if numbers:
-            price = max(numbers)
-            price = "£" + str(price)
+            price = "£" + str(max(numbers))
         else:
             print(f"No price found for {url}. Skipping...")
             return None
 
-        # Get Title
+        # Extract Title
         title_element = soup.find('h2', class_='margin-xs-0 flex-grow-1')
         title = title_element.get_text(strip=True) if title_element else "Placeholder Title"
         
-        #Get Date
+        # Extract Date
         date_element = soup.find('strong', class_='info')
         date = date_element.get_text() if date_element else "Placeholder Date"
         month = date.split(' ')[1].strip()
 
-        #Format Date
+        # Format Date
         date = parser.parse(date)
         formatted_date = date.strftime("%Y-%m-%d")
 
-        #Get Description
+        # Extract Description
         info_margin_elements = soup.find_all(class_="information margin-bottom-xs-64")
         body = [element.text.strip() for element in info_margin_elements]
-        markdown = ""
-        for item in body:
-            markdown += "- " + item.strip() + "\n\n"
+        markdown = "\n\n".join(f"- {item}" for item in body)
 
-        # Get Description
+        # Extract Description using regex
         pattern = r'<p class="desc preamble">(.*?)</p>'
         matches = re.findall(pattern, response.text, re.DOTALL)
         cleaned_text = [re.sub(r'<.*?>', '', match) for match in matches]
@@ -63,6 +62,7 @@ def extract_data(url):
         google_maps_link = soup.find("a", href=lambda href: href and "maps.google.com" in href)
 
         # Extract the href attribute value
+        embed_link = ""
         if google_maps_link:
             google_maps_link = google_maps_link["href"]
             lat_lng = google_maps_link.split('=')[1]
@@ -84,7 +84,6 @@ def extract_data(url):
         print(f"Error fetching data from {url}: {e}")
         return None
 
-# Create Post
 def generate_file(data, template_file, output_dir, id):
     try:
         # Check if data is None (indicating an error during extraction)
@@ -95,20 +94,17 @@ def generate_file(data, template_file, output_dir, id):
         with open(template_file, 'r') as file:
             template = file.read()
 
+        # Replace placeholders in the template with actual data
         output = template.format(**data)
+        output = output.replace("## Location", "## Location\n" + data['google'])
+        output = output.replace("## Sign Up", "## Sign Up\n" + data['register'])
+        output = output.replace("## Description", "## Description\n" + data['body'])
 
-        location_section = "## Location\n" + data['google']
-        output = output.replace("## Location", location_section)
-
-        register_section = "## Sign Up\n" + data['register']
-        output = output.replace("## Sign Up", register_section)
-
-        body_section = "## Description\n" + data['body']
-        output = output.replace("## Description", body_section)
-
+        # Format the title for the output file
         formatted_title = re.sub(r'\W+', '-', data['title'].lower().strip())
         output_file = f"{output_dir}/{id}-{formatted_title}.md"
 
+        # Write the output to a file
         with open(output_file, 'w') as file:
             file.write(output)
 
@@ -117,7 +113,7 @@ def generate_file(data, template_file, output_dir, id):
     except Exception as e:
         print(f"Error generating file: {e}")
 
-# Catch no URL error
+# Check for the presence of a command-line argument
 if len(sys.argv) < 2:
     print("Please provide the text file containing URLs as a command-line argument.")
     sys.exit(1)
@@ -130,9 +126,11 @@ with open(file_path, 'r') as url_file:
 template_file = 'src/templates/template.md'
 output_dir = 'content/posts'
 
+# Process each URL
 for url in urls:
     digits = re.findall(r'\d+', url)
     id = digits[-1]
 
+    # Extract data and generate a file for each URL
     data = extract_data(url)
     generate_file(data, template_file, output_dir, id)
